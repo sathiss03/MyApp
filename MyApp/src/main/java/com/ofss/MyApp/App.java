9b9fde905f1236a66f8ac33b88ccf86a97cb2d56
sathiss03 <sathiss_03@rediffmail.com>
2020-06-24 11:31:22 +0530

    1.0

---
+++ MyApp/src/main/java/com/ofss/MyApp/App.java
@@ -0,0 +1,15 @@
+package com.ofss.MyApp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        Class1 c=new Class1();
+        c.ok();
+    }
+}
---
+++ MyApp/src/main/java/com/ofss/MyApp/Class1.java
@@ -0,0 +1,8 @@
+package com.ofss.MyApp;
+
+public class Class1 {
+
+	public  void ok() {
+		System.out.println("we");
+	}
+}
